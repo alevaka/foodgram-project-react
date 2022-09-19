76aab32528f6926277603f0e4831a5185bac0159
Alexey Kachkaev <akachkaev@gmail.com>
2022-09-19 15:51:27 +0500

    Change usrers admin

--- backend/foodgram/users/admin.py
+++ backend/foodgram/users/admin.py
@@ -4,7 +4,8 @@ from users.models import User
 
 class UserAdmin(admin.ModelAdmin):
     list_display = ('pk', 'first_name', 'last_name', 'email')
-    search_fileds = ('first_name', 'email')
+    list_filter = ('first_name', 'email')
+    search_fields = ('first_name', 'email')
 
 
 class UserShoppingCart(User.shopping_cart.through):
